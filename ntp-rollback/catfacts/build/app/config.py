3c812c2d0ced9ad964a8b759642e820ba412f6ea
Victor "Nate" Graf <nategraf1@gmail.com>
2018-07-09 00:22:28 -0500

    delete ntp-rollback (segal)

--- ntp-rollback/catfacts/build/app/config.py
+++
@@ -1,8 +0,0 @@
-from os import environ
-
-bind = "0.0.0.0:443"
-workers = 1
-
-certfile = environ.get("CERT_PATH", "/etc/ssl/localhost.crt")
-keyfile = environ.get("KEY_PATH", "/etc/ssl/localhost.key")
-ciphers="TLSv1.2"
